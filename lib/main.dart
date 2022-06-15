2190668d9508557a87006a12a886aa2f34240bdd
Asadal_ <69556101+YunwooC@users.noreply.github.com>
2022-06-15 13:33:06 -0700

    Timer Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,85 @@
 import 'package:flutter/material.dart';
 
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: const Text(
-              "Pomostand!",
-            ),
-        ),
+      title: 'Pomodoro',
+      home: const IndexPage(title: 'IndexPage')
+    );
+  }
+}
+
+class IndexPage extends StatelessWidget {
+  const IndexPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(title),
+      ),
         
         // container encapsulates the column
-        body: Container(
+      body: Container(
           
-          margin: EdgeInsets.all(25),
-          color: Colors.lightGreen,        // green to see the container clearly
+        margin: EdgeInsets.all(25),       
+        decoration: const BoxDecoration(color: Colors.lightGreen),   // green to see the container clearly
           
           
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              const Text(
-                "POMOSTAND",
-                style: const TextStyle(fontSize: 40)
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Text(
+              "POMOSTAND",
+              style: const TextStyle(fontSize: 40),
+                
+            ),
+
+            Image.asset(
+              'assets/images/tomato.png',
+              fit: BoxFit.cover,
+            ),
+
+            TextButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return const TimerPage(title: 'Timer');
+                }));
+              },
+              child: const Text(
+                "CLICK ME",
+                style: const TextStyle(fontSize: 20)
               ),
-              Image.asset('assets/images/tomato.png'),
-              TextButton(
-                onPressed: null,
-                child: const Text(
-                  "CLICK ME",
-                  style: const TextStyle(fontSize: 20)
-                ),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
-                  padding: MaterialStateProperty.all(
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20)
-                  )
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                padding: MaterialStateProperty.all(
+                  const EdgeInsets.symmetric(vertical: 10, horizontal: 20)
                 )
-              ),
-            ]
-          )
+              )
+            ),
+          ]
         )
       )
+    );
+  }
+}
+
+class TimerPage extends StatelessWidget {
+  const TimerPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title)
+      ),
     );
   }
 }
